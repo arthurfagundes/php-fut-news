147059cccdd7c8fed39300a33ea6335778d43ded
arthurfagundes <arthuurfagundes@gmail.com>
2022-11-25 20:04:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,17 @@
     <link href="css/signin.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
+<style>
+    .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+    }
+</style>
 
 <body">
-    <div class="b-example-divider"></div>
     <header class="p-3 text-bg-black">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
@@ -24,13 +32,27 @@
                 <a href="login-form.php">
                     <button type="button" class="btn btn-outline-light me-2">Login</button>
                 </a>
-                <a href="register-form.php">
-                    <button type="button" class="btn btn-warning">Criar conta</button>
-                </a>
             </div>
         </div>
         </div>
     </header>
+
+    <div class="px-4 pt-5 my-5 text-center ">
+        <h1 class="display-4 fw-bold text-white">Bem vindo(a) a <span class="text-warning">Fut News!</span></h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4 text-white">Aqui você vai encontrar todos os melhores momentos da Copa do Mundo FIFA 2022, crie sua conta e fique por dentro de tudo! </p>
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
+                <a href="register-form.php">
+                    <button type="button" class="btn btn-warning btn-lg px-4 me-sm-3">Criar conta</button>
+                </a>
+            </div>
+        </div>
+        <div class="overflow-hidden" ">
+            <div class=" container px-5">
+            <img src="img/preview.png" class="img-fluid  rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
+        </div>
+    </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </body>
 
