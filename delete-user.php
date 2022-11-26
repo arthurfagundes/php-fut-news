196d078dd3f548e8f1daa431c0ce8ed0e46c84c8
arthurfagundes <arthuurfagundes@gmail.com>
2022-11-25 21:03:12 -0300

    Add delete-user.php

---
+++ delete-user.php
@@ -0,0 +1,16 @@
+<?php
+$id = $_GET['id'];
+
+try {
+    $connect = new PDO("mysql:host=localhost;dbname=bdfutnews", "root", "");
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connect->exec("set names utf8");
+    echo 'Connected';
+} catch (PDOException $erro) {
+    echo "Erro na conexão:" . $erro->getMessage();
+}
+
+$stmt = $connect->prepare("DELETE FROM users WHERE id = ?");
+$stmt->bindParam(1, $id);
+$stmt->execute();
+header('Location: ../admin-panel.php');
